7e306c065f389f6c38afdaac483739d238069911
WiktorIdziak <Witusaki@gmail.com>
2021-03-01 02:12:26 +0100

    Zadania 1-9, 22.02

---
+++ main.py
@@ -0,0 +1,79 @@
+#Zad.1
+'''napis1 = "napis1"
+napis2 = "napis2"
+lNaturalna1 = 5
+lNaturalna2 = 10
+lNzeczywista1 = 5.7
+lNzeczywista2 = 20.21
+lNespolona1 = lnaturalna1 + 5j
+lNespolona2 =lnaturalna2 + 7j
+print(napis1)
+print(napis2)
+print(lnaturalna1)
+print(lnaturalna2)
+print(lrzeczywista1)
+print(lrzeczywista2)
+print(lzespolona1)
+print(lzespolona2)'''
+#Zad.2
+'''a=10
+b=2
+dodawanie = a+b
+odejmowanie = a-b
+mnozenie = a*b
+dzielenie = a/b
+dzielenieCal = dodawanie//a
+reszta = dodawanie%a
+potega = a**b
+potega2 = pow(a,b)
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
+print(dzielenieCal)
+print(reszta)
+print(potega)
+print(potega2)'''
+#Zad.3
+'''a=5
+print(a)
+a+=1
+print(a)
+a-=1
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a**=a
+print(a)
+a%=2
+print(a)'''
+#Zad.4
+'''from math import exp,sin,log
+wynik1 = exp(10)
+wynik2 = pow(log(5+sin(8) ** 2),1.0/6.0)
+wynik3 = abs(4)-abs(0.45)
+wynik4 = abs(4)+abs(0.80)
+print(wynik1)
+print(wynik2)
+print(wynik3)
+print(wynik4)'''
+#Zad.5
+'''imie = "WIKTOR"
+nazwisko = "IDZIAK"
+print(imie.capitalize() + ' ' + nazwisko.capitalize())'''
+#Zad.6
+'''piosenka = "la la la la bumcyfykszy la la la la"
+print("la: ",piosenka.count("la"))'''
+#Zad.7
+'''zmienna = "persona"
+print(zmienna[1], zmienna[len(zmienna)-1])'''
+#Zad.8
+'''piosenka = "la la la la bumcyfykszy la la la la"
+print(piosenka.split())'''
+#Zad.9
+'''liczba = 11
+print("string: %s"% liczba)
+print("float: %f"% liczba)
+print("szesnastkowy: %X"% liczba)'''
